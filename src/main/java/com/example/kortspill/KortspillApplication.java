7ae993f13dc25e92dad9e169b63a0936f8902c83
“JarlM06” <jarl.ebbesen@outlook.com>
2024-04-08 08:49:54 +0200

    Initial commit

---
+++ src/main/java/com/example/kortspill/KortspillApplication.java
@@ -0,0 +1,13 @@
+package com.example.kortspill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KortspillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KortspillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kortspill/KortspillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kortspill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KortspillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
