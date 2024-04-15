900c2f49be6f274ebcd24547e8f55ecff057efa7
“JarlM06” <jarl.ebbesen@outlook.com>
2024-04-15 09:03:01 +0200

    Error handling

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,13 +2,13 @@ CREATE DATABASE IF NOT EXISTS databasenavn;
 USE databasenavn;
 
 CREATE TABLE IF NOT EXISTS Kortstokk (
-                                         id INT AUTO_INCREMENT PRIMARY KEY
+    id INT AUTO_INCREMENT PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS Kort (
-                                    id INT AUTO_INCREMENT PRIMARY KEY,
-                                    kortstokk_id INT,
-                                    type VARCHAR(255),
-                                    nummer INT,
-                                    FOREIGN KEY (kortstokk_id) REFERENCES Kortstokk(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    kortstokk_id INT,
+    type VARCHAR(255),
+    nummer INT,
+    FOREIGN KEY (kortstokk_id) REFERENCES Kortstokk(id)
 );
